7c4739649f290e7761538a78e34f66c195b00ec7
ImmortalNights <bitbucket@immortalnights.net>
2017-09-07 01:23:06 +0100

    implement core analyzer output begin cleaning up the core to improve usability, output and task flow

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const cmdLineArgs = require('./lib/cmdlineargs');
 const Player = require('./lib/player');
 const DataStore = require('./lib/datastore');
+const Analyzer = require('./lib/analyzer');
+const Steam = require('./lib/steam');
+// const moment = require('moment');
 const _ = require('underscore');
 
-const cmdLine = cmdLineArgs('title', 'description', [{
+const cmdLine = cmdLineArgs('title', 'description', [
+	{
 		name: 'user',
 		alias: 'u',
 		typeLabel: 'arg',
@@ -11,27 +15,12 @@ const cmdLine = cmdLineArgs('title', 'description', [{
 		defaultOption: true
 	},
 	{
-		name: 'apps',
-		alias: 'a',
-		typeLabel: 'args',
-		description: 'App IDs',
-		type: function(value) {
-			return value === '*' ? value : Number(value);
-		},
-		multiple: true
-	},
-	{
-		name: 'privateKey',
+		name: 'key',
 		typeLabel: 'arg',
 		description: 'Steam private API key'
 	},
 	{
 		name: 'refresh',
-		description: '',
-		type: Boolean
-	},
-	{
-		name: 'analyze',
 		description: '',
 		type: Boolean
 	},
@@ -75,12 +64,33 @@ class App
 
 	analyzeUser()
 	{
-		
+		var analyzer = new Analyzer(this._player, this._games);
+		// Display the user summary
+		console.log("Statistics:");
+		_.each(analyzer.statistics, function(value, key) {
+			console.log("%s %s", value, key);
+		});
+		console.log("");
+		// Display the ten easiest games
+		console.log("Easiest Games:");
+		var easiestGames = analyzer.getEasiestGames();
+		_.each(easiestGames, function(item) {
+			console.log(" - %s (%i): %s%%", item.name, item.id, item.globalCompletionPercentage.toFixed(2));
+		});
+		console.log("");
+		// Display the ten easiest achievements
+		console.log("Easiest Achievements:");
+		var easiestAchievements = analyzer.getEasiestAchievements(false);
+		_.each(easiestAchievements, function(item) {
+			console.log(" - %s (%i)", item.gameName, item.id);
+			console.log("   %s (%s%%)", item.name, item.globalCompletionPercentage.toFixed(2));
+		});
+		console.log("");
 	}
 
-	refreshUser(privateKey)
+	refreshUser(key)
 	{
-		var promise = this._player.refresh(privateKey);
+		var promise = this._player.refresh(key);
 
 		promise.then(() => {
 			this._player.save();
@@ -89,7 +99,7 @@ class App
 		return promise;
 	}
 
-	refreshDataStore(privateKey, apps)
+	refreshDataStore(key, apps)
 	{
 		if (apps.length === 1 && apps[0] === '*')
 		{
@@ -109,9 +119,9 @@ class App
 		// console.log("apps", apps);
 
 		var promise;
-		if (!_.isEmpty(apps) && privateKey)
+		if (!_.isEmpty(apps) && key)
 		{
-			promise = this._games.fetch(privateKey, apps);
+			promise = this._games.fetch(key, apps);
 		}
 		else
 		{
@@ -133,55 +143,178 @@ class App
 	}
 }
 
+var task_GetPlayerAchievementsForGames = function(user, games) {
+	console.log("Get user achievements for %i game(s)...", games.length);
 
+	var self = this;
+	return new Promise(function(resolve, reject) {
+		var failures = 0;
+		var check = _.after(games.length, function() {
+			// reject parent promise if all requests failed
+			if (games.length === failures)
+			{
+				reject();
+			}
+			else
+			{
+				resolve(games);
+			}
+		});
+
+		// Some requests may fail if the game no longer exists or has no associated data
+		// if the game does not have achievements it should be marked so that it's not requested again
+		var requests = _.map(games, function(game) {
+			return self.getPlayerAchievementsForGame(user, game)
+			.then(check)
+			.catch(function(err) {
+				++failures;
+				check();
+			});
+		});
+	});
+}
 
 // console.log("Options", options);
 
-if (options.help || !options.user && !options.apps)
+if (options.help || !options.user)
 {
 	console.log(cmdLine.usage());
 }
 else
 {
-	var app = new App();
+	// Always have to load the initialize store
+	let games = new DataStore('games.json');
+	let user = new Player(options.user);
 
-	app.init('games.json', options.user)
-	.then(function() {
-		console.log("next");
-		var promise = null;
+	Promise.all([games.load(), user.load()])
+	.then(function(results, b) {
+		let games = results[0];
+		let user = results[1];
 
-		if (options.refresh)
+		if (options.key)
 		{
-			if (options.apps)
-			{
-				promise = app.refreshDataStore(options.privateKey, options.apps);
-			}
-			else if (options.user)
-			{
-				promise = app.refreshUser(options.privateKey);
-			}
-			else
-			{
-				console.error("Must provide user or apps to refresh");
-			}
-		}
-		else if (options.analyze)
-		{
-			if (options.user)
-			{
-				app.analyzeUser();
-			}
-			else
-			{
-				console.error("Must provide user to analyze");
-			}
-		}
+			var steam = new Steam(options.key);
+			
+			console.log("Retrieving owned games...");
+			steam.getOwnedGames(user.id)
+			.then(function(ownedGames) {
+				// fetch any / all games which the user has, which the data store does not have
+				// TODO fetch any games which are over 1 month old
+				// Identify new games for statistical purposes
+				// TODO determine how long ago the data was refreshed and remember the game count change
+				var newGames = _.filter(ownedGames, function(game) { return !_.has(user.data, game.appid); });
+				console.log("Identified", newGames.length, "new games.");
 
-		return promise;
+				// Add all new games to the user data
+				_.each(newGames, function(game) {
+					user.data[game.appid] = _.omit(game, 'appid');
+				});
+
+				// If the user has played them, get their achievement information
+				var requiredGames = _.filter(newGames, function(game) { return game.playtime_forever !== 0; });
+
+				var promise;
+				if (!_.isEmpty(requiredGames))
+				{
+					promise = task_GetPlayerAchievementsForGames.call(steam, user.id, requiredGames)
+					.then(function(game) {
+						_.each(games, function(game) {
+							// update the game data
+							_.extend(user.data[game.appid], _.omit(game, 'appid'));
+						});
+
+						console.log("Saving updated user data...");
+						return user.save().catch(function(err) {
+							console.error("Failed to save updated user data!", err)
+						});
+					})
+					.catch(function(err) {
+						console.error("Failed to retrieve any game achievement data");
+					});
+				}
+
+				return promise;
+			})
+			.then(function() {
+				console.log("")
+			})
+			.catch(function(err) {
+				console.log("Failed to get user games.");
+				console.log(err);
+			});
+		}
+		else
+		{
+			_.defer(function() {
+				var analyzer = new Analyzer(user, games);
+				// Display the user summary
+				console.log("Statistics:");
+				_.each(analyzer.statistics, function(value, key) {
+					console.log("%s %s", value, key);
+				});
+				console.log("");
+				// Display the ten easiest games
+				console.log("Easiest Games:");
+				var easiestGames = analyzer.getEasiestGames();
+				_.each(easiestGames, function(item) {
+					console.log(" - %s (%i): %s%%", item.name, item.id, item.globalCompletionPercentage.toFixed(2));
+				});
+				console.log("");
+				// Display the ten easiest achievements
+				console.log("Easiest Achievements:");
+				var easiestAchievements = analyzer.getEasiestAchievements(false);
+				_.each(easiestAchievements, function(item) {
+					console.log(" - %s (%i)", item.gameName, item.id);
+					console.log("   %s (%s%%)", item.name, item.globalCompletionPercentage.toFixed(2));
+				});
+				console.log("");
+			});
+		}
 	})
 	.catch(function(err) {
-		console.log("Failed", err);
+		console.error("Failed to initialize game store or user");
+		console.error(err);
 	});
+
+	// var app = new App();
+
+	// app.init(, options.user)
+	// .then(function() {
+	// 	console.log("next");
+	// 	var promise = null;
+
+	// 	if (options.refresh)
+	// 	{
+	// 		if (options.apps)
+	// 		{
+	// 			promise = app.refreshDataStore(options.key, options.apps);
+	// 		}
+	// 		else if (options.user)
+	// 		{
+	// 			promise = app.refreshUser(options.key);
+	// 		}
+	// 		else
+	// 		{
+	// 			console.error("Must provide user or apps to refresh");
+	// 		}
+	// 	}
+	// 	else
+	// 	{
+	// 		if (options.user)
+	// 		{
+	// 			app.analyzeUser();
+	// 		}
+	// 		else
+	// 		{
+	// 			console.error("Must provide user to analyze");
+	// 		}
+	// 	}
+
+	// 	return promise;
+	// })
+	// .catch(function(err) {
+	// 	console.log("Failed", err);
+	// });
 
 
 	// if (options.apps)
@@ -212,9 +345,9 @@ else
 	// 	.then(function() {
 	// 		if (options.refresh)
 	// 		{
-	// 			if (options.privateKey)
+	// 			if (options.key)
 	// 			{
-	// 				player.refresh(options.privateKey)
+	// 				player.refresh(options.key)
 	// 				.then(function() {
 	// 					return player.save();
 	// 				})
